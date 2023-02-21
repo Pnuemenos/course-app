4d10e12e77e91663facc5916ee5aae3da7d17cd2
Pnuemenos <dhiren.master@hrclarity.ai>
2023-02-21 12:14:55 +0000

    Trying to revert change

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,17 +14,17 @@ export default function Home() {
         </p>
         <div>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://hrclarity.ai"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/BlobLogo.png"
+              alt="HRClarity Logo"
               className={styles.vercelLogo}
               width={100}
-              height={24}
+              height={100}
               priority
             />
           </a>
@@ -34,15 +34,12 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src="/HRClarityLogo.png"
+          alt="HRClarity.Ai logo"
+          width={300}
+          height={150}
           priority
         />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
       </div>
 
       <div className={styles.grid}>
@@ -87,5 +84,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
